26a84801f015296f6c63dee68630a9b585122b1f
dannyelzein <elzeindanny5@gmail.com>
2023-09-30 21:49:36 -0400

    Refactored code

--- src/validator.java
+++ src/validator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class validator {
     public static void main(String[] args) {
         int sumOddDigits = 0;
         int sumEvenDigits = 0;
@@ -12,23 +12,23 @@ public class Main {
 
        String reverseCard = String.valueOf(new StringBuilder(card).reverse());
 
-       for(int i = 0; i<reverseCard.length(); i+=2){
+       for(int i = 0; i < reverseCard.length(); i += 2){
            sumOddDigits += Character.getNumericValue(reverseCard.charAt(i));
        }
 
-        for(int i = 1; i<reverseCard.length(); i+=2) {
-            int x = Character.getNumericValue(reverseCard.charAt(i))*2;
-            if(x>=10){
-                sumEvenDigits+= (1+(x%10));
+        for(int i = 1; i < reverseCard.length(); i += 2) {
+            int x = Character.getNumericValue(reverseCard.charAt(i)) * 2;
+            if(x >= 10){
+                sumEvenDigits+= (1 + (x % 10));
             }
             else {
-                sumEvenDigits +=x;
+                sumEvenDigits += x;
             }
         }
 
         total = sumEvenDigits + sumOddDigits;
 
-        if (total%10 == 0){
+        if (total % 10 == 0){
             System.out.println("Valid");
         }
         else {
